9a11f4c7ade9f58c83ddf44cbe93a507084e5152
Niko Hienonen <niko.hienonen@cs.tamk.fi>
2019-07-07 13:23:26 +0300

    build done

---
+++ build/precache-manifest.6d696f8aacd1fee30e3d69fc8f557b65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "904a28894c2de4c9575a3a4c34d3c32c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6d65d9a13f9e3de66bf",
+    "url": "/static/css/main.c0e0b27a.chunk.css"
+  },
+  {
+    "revision": "7a5268c61cea1fe0e1b1",
+    "url": "/static/js/2.75d3e3e0.chunk.js"
+  },
+  {
+    "revision": "b6d65d9a13f9e3de66bf",
+    "url": "/static/js/main.71697678.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- frontend/src/Coursecontext.js
+++ frontend/src/Coursecontext.js
@@ -1,12 +1,133 @@
 import React from 'react'
-import axios from 'axios';
 
 const CourseContext = React.createContext()
 
 export const CourseConsumer = CourseContext.Consumer
 
 class CourseProvider extends React.Component {
-  state = {url: 'http://localhost:8080'}
+  state = {
+    courses: [
+      {id: 4324
+        , name: "Drum Mics"
+        , category: "Recording"
+        , span: 1
+        , teacher: "Andy Mattson"
+        , tags: ["recording", "drums", "mattson"]
+      }
+      , {id: 4624
+        , name: "Substractive EQ"
+        , category: "Mixing"
+        , span: 1
+        , teacher: "Madison Hill"
+        , tags: ["mixing", "eq", "hill"]
+      }
+      , {id: 5323
+        , name: "Guitar re-amp"
+        , category: "Recording"
+        , span: 1
+        , teacher: "David Lowe"
+        , tags: ["recording", "guitar", "lowe"]
+      }
+      , {id: 2352
+        , name: "What is Mastering?"
+        , category: "Mastering"
+        , span: 1
+        , teacher: "Nick Finen"
+        , tags: ["mastering", "mastering", "finen"]
+      }
+      , {id: 7646
+        , name: "Echo effects"
+        , category: "Mixing"
+        , span: 2
+        , teacher: "Donald Kingdon"
+        , tags: ["mixing", "echo", "kingdon"]
+      }
+      , {id: 9305
+        , name: "Metal Compression"
+        , category: "Mixing"
+        , span: 1
+        , teacher: "Sander Taylor"
+        , tags: ["mixing", "compression", "taylor"]
+      }
+      , {id: 6345
+        , name: "Room Mics"
+        , category: "Recording"
+        , span: 2
+        , teacher: "Nick Finen"
+        , tags: ["recording", "room", "finen"]
+      }
+      , {id: 1005
+        , name: "Starting with MIDI"
+        , category: "Mixing"
+        , span: 1
+        , teacher: "Hannah Tap"
+        , tags: ["mixing", "midi", "tap"]
+      }
+      , {id: 3334
+        , name: "Studio Coziness"
+        , category: "Studio"
+        , span: 2
+        , teacher: "Mark Pine"
+        , tags: ["studio", "cozy", "pine"]
+      }
+      , {id: 6255
+        , name: "Loudness Wars"
+        , category: "Mastering"
+        , span: 1
+        , teacher: "Christina Lyone"
+        , tags: ["mastering", "loudness", "lyone"]
+      }
+      , {id: 5455
+        , name: "DAW Selection"
+        , category: "Misc"
+        , span: 1
+        , teacher: "Mark Pine"
+        , tags: ["misc", "daw", "pine"]
+      }
+      , {id: 4666
+        , name: "Vocal Recording"
+        , category: "Recording"
+        , span: 2
+        , teacher: "Andy Mattson"
+        , tags: ["recording", "vocals", "mattson"]
+      }
+      , {id: 6549
+        , name: "Stereo Image"
+        , category: "Mixing"
+        , span: 1
+        , teacher: "Maximillian Dolt"
+        , tags: ["mixing", "stereo", "Dolt"]
+      }
+      , {id: 1112
+        , name: "Mixing Metal Guitars"
+        , category: "Mixing"
+        , span: 2
+        , teacher: "Sander Taylor"
+        , tags: ["mixing", "guitars", "metal", "taylor"]
+      }
+      , {id: 5311
+        , name: "Choir Recording"
+        , category: "Recording"
+        , span: 2
+        , teacher: "Andy Mattson"
+        , tags: ["recording", "choir", "mattson"]
+      }
+      , {id: 4242
+        , name: "Basics of Recording"
+        , category: "Recording"
+        , span: 3
+        , teacher: "Andy Mattson"
+        , tags: ["recording", "basics", "mattson"]
+      }
+      , {id: 6454
+        , name: "Top-Down Mixing"
+        , category: "Mixing"
+        , span: 1
+        , teacher: "Nick Finen"
+        , tags: ["työhyvinvointi", "finen"]
+      }
+    ]
+  }
   componentDidMount() {
     this.checkIfFirstSignIn();
   }
@@ -18,10 +139,7 @@ class CourseProvider extends React.Component {
     }
   }
   getCourses = (callback) => {
-    axios.get(this.state.url+'/courses')
-    .then(result => {
-      callback(result.data);
-    })
+    callback(this.state.courses);
   }
   setStringToNumArray = (string) => {
     if(string) {
@@ -44,10 +162,10 @@ class CourseProvider extends React.Component {
     }
   }
   getCourseByID = (id, callback) => {
-    axios.get('/courses/'+id)
-    .then(result => {
-      callback(result.data);
-    });
+    let found = this.state.courses.find(course => {
+      return course.id === Number(id);
+  })
+  callback(found); 
   }
   signUpForACourse = (id, callback) => {
     let storage = sessionStorage.getItem("userCourses");
