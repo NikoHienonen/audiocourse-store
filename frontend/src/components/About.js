bc7bac5c5faf501d60e9910a0305ff04083748c0
Niko Hienonen <niko.hienonen@cs.tamk.fi>
2019-07-08 10:22:54 +0300

    fixed about styling

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -5,15 +5,17 @@ const About = () => {
     <div className="content">
       <div className="generic-container">
         <h1>About</h1>
-        <div className="contact-card">
-          <p>This is a placeholder app which I created from a "secret" app
-            I was asked to make for a client, and since I was quite happy with
-            the result I wanted to make a "generic" version of it with dummy data
-            and different theme so that I can showcase it in my github.
-          </p>
-        </div>
-        <div className="contact-card">
-        <i className="fab fa-github"></i> https://github.com/NikoHienonen
+        <div className="contacts">
+          <div className="contact-card">
+            <p>This is a placeholder app which I created from a "secret" app
+              I was asked to make for a client, and since I was quite happy with
+              the result I wanted to make a "generic" version of it with dummy data
+              and different theme so that I can showcase it in my github.
+            </p>
+          </div>
+          <div className="contact-card">
+            <i className="fab fa-github"></i> https://github.com/NikoHienonen
+          </div>
         </div>
       </div>
     </div>
