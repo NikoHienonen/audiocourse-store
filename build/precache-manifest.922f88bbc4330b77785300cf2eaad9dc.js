9802b2b7b060825746a89119938f65930ae2b339
Niko Hienonen <niko.hienonen@cs.tamk.fi>
2019-07-08 10:23:12 +0300

    fixed styling

---
+++ build/precache-manifest.922f88bbc4330b77785300cf2eaad9dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c55bf9861f9c56bd16e084f999b8a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53cd335429ae552ac05f",
+    "url": "/static/css/main.c0e0b27a.chunk.css"
+  },
+  {
+    "revision": "1ad4f11eef9baffa8262",
+    "url": "/static/js/2.b03b5ae9.chunk.js"
+  },
+  {
+    "revision": "53cd335429ae552ac05f",
+    "url": "/static/js/main.b6fe8928.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
